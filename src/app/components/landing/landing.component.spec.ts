b31fb89ea85d0e6d074697565531f40336be8ba3
denivladislav <deni.vladislav@gmail.com>
2025-01-05 13:30:55 +0300

    feat: routing + navigation test added

--- src/app/components/landing/landing.component.spec.ts
+++ src/app/components/landing/landing.component.spec.ts
@@ -1,22 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingComponent } from './landing.component';
+import { Router } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../app.routes';
+import { By } from '@angular/platform-browser';
 
 describe('LandingComponent', () => {
     let component: LandingComponent;
     let fixture: ComponentFixture<LandingComponent>;
+    let router: Router;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [LandingComponent],
+            providers: [provideRouter(routes)],
         }).compileComponents();
 
         fixture = TestBed.createComponent(LandingComponent);
         component = fixture.componentInstance;
+        router = TestBed.inject(Router);
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should navigate', async () => {
+        fixture.debugElement.query(By.css('a[href="/converter"]')).nativeElement.click();
+
+        await fixture.whenStable();
+        expect(router.url).toBe('/converter');
     });
 });
