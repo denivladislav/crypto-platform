1459ad4546695279b7140324bd14bea427fe730d
denivladislav <deni.vladislav@gmail.com>
2025-01-12 17:04:00 +0300

    fix: fix underscoreToNormalCase pipe test

--- src/app/pipes/underscore-to-normal-case/underscore-to-normal-case.pipe.spec.ts
+++ src/app/pipes/underscore-to-normal-case/underscore-to-normal-case.pipe.spec.ts
@@ -1,14 +1,19 @@
 import { UnderscoreToNormalCasePipe } from './underscore-to-normal-case.pipe';
 
 describe('UnderscoreToNormalCasePipe', () => {
-    const dict: Record<string, string> = {
-        hello: 'hello',
-        hello_world: 'hello world',
-        '': '',
-    };
+    let pipe: UnderscoreToNormalCasePipe;
+    let dict: Record<string, string>;
+
+    beforeEach(() => {
+        pipe = new UnderscoreToNormalCasePipe();
+        dict = {
+            hello: 'hello',
+            hello_world: 'hello world',
+            '': '',
+        };
+    });
 
     it('should convert underscore to normal case', () => {
-        const pipe = new UnderscoreToNormalCasePipe();
         Object.keys(dict).forEach((key) => {
             expect(pipe.transform(key)).toEqual(dict[key]);
         });
